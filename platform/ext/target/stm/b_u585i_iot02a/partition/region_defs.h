98e57110a9b82c5563de72e798565b7ef6d5506a
xijian zhu <xijian.zhu@quantropi.com>
2026-03-02 08:54:32 -0500

    PE-2844 change partition

--- platform/ext/target/stm/b_u585i_iot02a/partition/region_defs.h
+++ platform/ext/target/stm/b_u585i_iot02a/partition/region_defs.h
@@ -18,8 +18,8 @@
 #define __REGION_DEFS_H__
 #include "flash_layout.h"
 
-#define BL2_HEAP_SIZE           0x000a000
-#define BL2_MSP_STACK_SIZE      0x0020000
+#define BL2_HEAP_SIZE           0x001c000
+#define BL2_MSP_STACK_SIZE      0x0011000
 
 #define LOADER_NS_MSP_STACK_SIZE 0x0000400
 #define LOADER_NS_HEAP_SIZE      0x0000200
