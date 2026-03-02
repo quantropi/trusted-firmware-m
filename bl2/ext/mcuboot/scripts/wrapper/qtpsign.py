4c6c5cc543404266b7312afc04ab09f6bc6fb238
xijian zhu <xijian.zhu@quantropi.com>
2026-03-02 14:03:25 -0500

    PE-2844 turn on encryption

--- bl2/ext/mcuboot/scripts/wrapper/qtpsign.py
+++ bl2/ext/mcuboot/scripts/wrapper/qtpsign.py
@@ -86,8 +86,8 @@ def wrap(sign_tool, align, version, header_size, layout, infile, outfile, measur
     else:
         cmd = "{}/qtpsign -v {} -t b_u585i_iot02a -s {} -h {} -a {} -k {}/sbl_certs/{}/customer.key -c {}/sbl_certs/{}/customer.crt -i {} -o {}".format(sign_tool, version, slot_size, header_size, align, sign_tool, sign_alg, sign_tool, sign_alg, infile, outfile)
   
-#    if encrypt is not None:
-#        cmd = cmd + " -e{}".format(encrypt)
+    if encrypt is not None:
+        cmd = cmd + " -e{}".format(encrypt)
 
     print("cmd", cmd)
     os.system(cmd)
