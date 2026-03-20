5c48460e71492ba186bd60cfa773db3bf8cafbfa
xijian zhu <xijian.zhu@quantropi.com>
2026-03-20 14:52:16 -0400

    PE-2832 mldsa87 revert

--- platform/ext/target/stm/b_u585i_iot02a/config.cmake
+++ platform/ext/target/stm/b_u585i_iot02a/config.cmake
@@ -10,7 +10,7 @@
 
 ################################## BL2 #########################################################################################################
 set(MCUBOOT_IMAGE_NUMBER                   2           CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each seperately")
-set(BL2_TRAILER_SIZE                       0x4000      CACHE STRING    "Trailer size")
+set(BL2_TRAILER_SIZE                       0x8000      CACHE STRING    "Trailer size")
 set(MCUBOOT_ALIGN_VAL                      16          CACHE STRING    "Align option to build image with imgtool")
 set(MCUBOOT_UPGRADE_STRATEGY      "SWAP_USING_SCRATCH" CACHE STRING    "Upgrade strategy for images")
 set(TFM_PARTITION_PLATFORM                 ON          CACHE BOOL      "Enable platform partition")
